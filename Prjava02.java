2119f05ff9021016f7f31804256aeb2b67fe4538
joanar2021daw2 <joanar2021daw2@protonmail.com>
2020-12-14 18:26:33 +0100

    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -29,6 +29,7 @@ public class Prjava02 {
             System.out.println("Versió OS: " + System.getProperty("os.version"));
 			System.out.println("Creació d'una branca del projecte prjava02");
 			System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+			System.out.println("Afegint més codi a la branca01 del projecte prjava02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
